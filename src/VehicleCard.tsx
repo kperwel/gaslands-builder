ef035d3a0a725da2f3bbb7d5a6d9e094f93c1f77
Marc Löhe <marc@marcloehe.de>
2019-12-17 23:06:18 +0100

    feat: display vehicle properties more compact

--- src/VehicleCard.tsx
+++ src/VehicleCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, ButtonGroup, Card, HTMLTable } from "@blueprintjs/core";
+import { Button, ButtonGroup, Card, HTMLTable, Tag } from "@blueprintjs/core";
 import { ActiveVehicle } from "./library/vehicles";
 import styles from "./VehicleCard.module.css";
 
@@ -17,43 +17,46 @@ const VehicleCard: React.FC<VehicleCardProps> = ({
   return (
     <Card>
       <h2>{vehicle.type.name}</h2>
-      <HTMLTable interactive>
-        <tbody>
-          <tr>
-            <td>Weight</td>
-            <td>{vehicle.type.weight}</td>
-          </tr>
-          <tr>
-            <td>Hull</td>
-            <td>{vehicle.type.hull}</td>
-          </tr>
-          <tr>
-            <td>Handling</td>
-            <td>{vehicle.type.handling}</td>
-          </tr>
-          <tr>
-            <td>Max. Gear</td>
-            <td>{vehicle.type.maxGear}</td>
-          </tr>
-          <tr>
-            <td>Crew</td>
-            <td>{vehicle.type.crew}</td>
-          </tr>
-          <tr>
-            <td>Build Slots</td>
-            <td>{vehicle.type.buildSlots}</td>
-          </tr>
-          <tr>
-            <td>Cost</td>
-            <td>{vehicle.type.cost}</td>
-          </tr>
-          {vehicle.type.specialRule && (
-            <tr className={styles.specialRule}>
-              <td colSpan={2}>{vehicle.type.specialRule}</td>
-            </tr>
-          )}
-        </tbody>
-      </HTMLTable>
+      {[
+        {
+          value: vehicle.type.weight
+        },
+        {
+          label: 'Hull',
+          value: vehicle.type.hull
+        },
+        {
+          label: 'Handling',
+          value: vehicle.type.handling
+        },
+        {
+          label: 'Max. Gear',
+          value: vehicle.type.maxGear
+        },
+        {
+          label: 'Crew',
+          value: vehicle.type.crew
+        },
+        {
+          label: 'Build Slots',
+          value: vehicle.type.buildSlots
+        },
+        {
+          label: 'Cost',
+          value: vehicle.type.cost
+        }
+      ].map(({ label, value }) => (
+        <div className={styles.propertyTag}>
+          <Tag>{label ? `${label}: ${value}` : value}</Tag>
+        </div>
+      ))}
+
+      {vehicle.type.specialRule && (
+        <div className={styles.specialRule}>
+          <td colSpan={2}>{vehicle.type.specialRule}</td>
+        </div>
+      )}
+
       <ButtonGroup>
         <Button icon="remove" onClick={onRemove}>
           Remove
