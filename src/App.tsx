939fb6e275859c09bfd02dd2f32855bc758555ac
Marc Löhe <marc@marcloehe.de>
2019-12-17 14:31:02 +0100

    feat: added layout basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import nanoId from 'nanoid';
 
-const App: React.FC = () => {
+interface ActiveVehicle {
+  id: string
+}
+
+interface AddVehicleAction {
+  type: 'addVehicle'
+  vehicle: ActiveVehicle
+}
+
+interface RemoveVehicleAction {
+  type: 'removeVehicle'
+  vehicleId: string
+}
+
+type VehicleAction = AddVehicleAction | RemoveVehicleAction;
+
+const App: React.FC = (): React.ReactElement => {
+
+  const reducer = (state: ActiveVehicle[], action: VehicleAction) => {
+    switch (action.type) {
+      case 'addVehicle':
+        return [...state, action.vehicle];
+      case 'removeVehicle':
+        return state.filter(({id}) => id !== action.vehicleId);
+      default:
+        throw new Error(`unknown vhicle reducer action: ${action}`);
+    }
+  };
+
+  const [vehicles, dispatchVehicleAction] = React.useReducer(reducer, []);
+
+  const addVehicle = (vehicle: ActiveVehicle) => {
+    dispatchVehicleAction({type: 'addVehicle', vehicle});
+  };
+
+  const removeVehicle = (vehicleId: string) => {
+    dispatchVehicleAction({type: 'removeVehicle', vehicleId: vehicleId});
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.wrapper}>
+      <header className={styles.header}>
+        <h1>Gaslands Builder</h1>
       </header>
+      <main className={styles.main}>
+        <div className={styles.controls}>
+          <button onClick={() => addVehicle({id: nanoId()})}>Add vehicle
+          </button>
+        </div>
+        <div className={styles.vehiclesContainer}>
+          {vehicles.map(vehicle => (
+            <div className={styles.vehicle}>
+              Vehicle {vehicle.id}
+              <button onClick={() => removeVehicle(vehicle.id)}>Remove</button>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
